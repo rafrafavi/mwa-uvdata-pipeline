65ade0ad78cb0285f8c38f499674508c8b1ba648
rafael <30427560+rafrafavi@users.noreply.github.com>
2025-04-15 17:17:14 +0800

    refactor: moved 03_mwalib functions into utils
    In general, I will follow a strategy whereby if a stage
    in the demo is imported as a module, it will be refactored
    into this lib.

--- src/mwa_utils/utils.py
+++ src/mwa_utils/utils.py
@@ -4,6 +4,47 @@ import functools
 from pathlib import Path
 
 from astropy.time import Time
+from mwalib import MetafitsContext
+from pandas import DataFrame
+
+
+def get_channel_df(ctx: MetafitsContext) -> DataFrame:
+    """Get channel information from metafits as a DataFrame."""
+    header = [
+        "gpubox_number",
+        "rec_chan_number",
+        "chan_start_hz",
+        "chan_centre_hz",
+        "chan_end_hz",
+    ]
+    df = DataFrame(
+        {h: [getattr(c, h) for c in ctx.metafits_coarse_chans] for h in header}
+    )
+    return df
+
+
+def get_antenna_df(ctx: MetafitsContext) -> DataFrame:
+    """Get antenna information from metafits as a DataFrame."""
+    header = [
+        "ant",
+        "tile_id",
+        "tile_name",
+        "electrical_length_m",
+        "east_m",
+        "north_m",
+        "height_m",
+    ]
+    df = DataFrame({h: [getattr(a, h) for a in ctx.antennas] for h in header})
+    df["flagged"] = [a.rfinput_x.flagged | a.rfinput_y.flagged for a in ctx.antennas]
+    # get elements from antenna rfinput_x, assuming it's the same as rfinput_y
+    rfheader = ["rec_number", "flavour", "has_whitening_filter"]
+    for h in rfheader:
+        df[h] = [getattr(a.rfinput_x, h) for a in ctx.antennas]
+    # rec_type is "ReceiverType.RRI", I want just "RRI"
+    df["rec_type"] = [
+        str(a.rfinput_x.rec_type).replace("ReceiverType.", "") for a in ctx.antennas
+    ]
+    return df
 
 
 @functools.lru_cache(maxsize=128)
